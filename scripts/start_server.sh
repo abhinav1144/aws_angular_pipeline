b006d3495baef4d41f6564f6ecf6177775cd36b2
abhinav1144 <76483898+abhinav1144@users.noreply.github.com>
2021-08-25 16:06:59 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@ cd /home/ubuntu/dotx/dotx-portal-ui
 sudo npm install
 sudo npm run build
 sudo service nginx stop
-sudo ./nginx -g 'daemon on; master_process on;'
+sudo /usr/sbin/nginx -g 'daemon on; master_process on;'
